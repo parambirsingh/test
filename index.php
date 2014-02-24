494bfbee4e1bb9c588f829b89ce5ace27a0f0e22
Parambir Singh <parambir@rudrainnovatives.com>
2014-02-24 02:26:53 -0800

    This is a test commit

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<html>
-<head>
-<title>My GitHub project</title>
-</head>
-<body>
-Hi, i am Parambir Singh. This is my first GitHub project.
-updated data
-<div>I am from Jammu</div>
-<div>I am web developer in RIS Mohali</div>
-</body>
-</html>
+Hi Harsih How are u
