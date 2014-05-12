697db65f46e349174d484144afa796b175bca962
Parambir Singh <parambir@rudrainnovatives.com>
2014-05-12 15:22:46 +0530

    This is new HTML Table that i am committed

--- abc.php
+++ abc.php
@@ -1,1 +1,14 @@
-my new file contents
+<html>
+	<head>
+		<title>Test</title>
+	</head>
+	<body>
+		<table>
+			<thead><tr><th>First</th><th>Second</th><th>Third</th></tr></thead>
+			<tbody><tr><td>1</td><td>2</td><td>3</td></tr>
+				<tr><td>4</td><td>5</td><td>6</td></tr>
+			</tbody>
+		</table>
+	</body>
+	
+</html>
